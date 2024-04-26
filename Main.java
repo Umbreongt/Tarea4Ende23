000ad3fb825f3812c394cb38c327874eab3dfba0
David <davidgomeztejeda12@gmail.com>
2024-04-26 10:05:43 +0200

    v2_26/04/2024

--- Main.java
+++ Main.java
@@ -3,29 +3,50 @@
  * and open the template in the editor.
  */
 
-package tarea4endes23;
+package EntidadFinanciera;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta objetoCuenta = null;
         double saldoActual;
         
-
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        try {
+            probarRetirar(objetoCuenta,666);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            probarIngresar(objetoCuenta, 999);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
         
     
     }
     
-    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+    public static void probarIngresar(CCuenta c, int cantidad, String cantidadEsperada) throws Exception{
         c.ingresar(cantidad);
-        
+        System.out.println("La cantidad esperada es : " + cantidadEsperada);
     }
     
-    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+    public static void probarRetirar(CCuenta c, int cantidad, String cantidadEsperada) throws Exception{
         c.retirar(cantidad);
+        System.out.println("La cantidad esperada es : " + cantidadEsperada);
+    }
+
+    private static void probarRetirar(CCuenta objetoCuenta, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void probarIngresar(CCuenta objetoCuenta, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
